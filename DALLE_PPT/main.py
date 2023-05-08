28e31ceb8ec9a63b6747e235f0483d56228d97c4
fullstackdev710 <fullstack710dev@gmail.com>
2023-05-07 23:49:54 -0400

    Get Slides for YOur DALL-E Application

--- DALLE_PPT/main.py
+++ DALLE_PPT/main.py
@@ -1,8 +1,30 @@
 import tkinter as tk
+from pptx import Presentation
+from pptx.utill import Inches
+
+
+def slide_generator(text, prs):
+    pass
 
 
 def get_slides():
-    print("Hi you are creating PPT slides.")
+    """
+    This is paragraph 1
+
+    This is a paragraph 2
+    ["This is paragraph 1", "This is a paragraph 2"]
+    """
+    text = text_field.get("1.0", "end-1c")
+    paragraphs = text.split("\n\n")
+    prs = Presentation()
+    width = Pt(1920)
+    height = Pt(1080)
+    prs.slide_width = width
+    prs.slide_height = height
+    for paragraph in paragraphs:
+        slide_generator(paragraph, prs)
+
+    prs.save("my_presentation.pptx")
 
 
 app = tk.Tk()
